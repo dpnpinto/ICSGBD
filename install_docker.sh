f429f19c8e5d4e19511a7c1341a0339e3d64b535
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2022-11-26 11:02:47 -0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+#
+# Author : Duarte Pinto
+# Date: November 2022
+# Version 1.0.0: "Install Docker"
+#
 sudo dnf check-update
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io
